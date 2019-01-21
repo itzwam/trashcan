b8fea5b1fb4b8779a11ab8552d4eb84356dbc1c7
iTzWam <antoine@MacBookPro-Antoine>
2019-01-21 16:14:16 +0100

    General commit (no IDs open) - Modifications for bad implementations

--- python-sandbox/ANSIplayground/spinning.py
+++ python-sandbox/ANSIplayground/spinning.py
@@ -5,12 +5,12 @@ def spinning(stop=50):
     t1 = datetime.now()
     spin = ['-','\\','|', '/']
     index = 0
-    sys.stdout.write(u"\u001b[?25l")
+    sys.stdout.write(u"\u001b[?25l") # Hide cursor
     while (datetime.now()-t1).seconds <= stop:
         index = index+1 if index < len(spin)-1 else 0
-        sys.stdout.write(u"\u001b[1000D" + spin[index])
+        sys.stdout.write(u"\u001b[1000D" + spin[index]) # Spiiiiiiiiiiiiiin
         sys.stdout.flush()
         time.sleep(0.1)
-    sys.stdout.write(u"\u001b[?25h")
+    sys.stdout.write(u"\u001b[?25h") # Show cursor
 
 spinning(2.5)
